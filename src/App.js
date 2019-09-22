950950a7bc3fb435c7a9d9336a4068f97d0fbe84
Christian Dimitrov <christian_dimitrov@abv.bg>
2019-09-22 23:26:25 +0300

    adding missing imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Alert from "./components/Alert";
 import ExpenseForm from "./components/ExpenseForm";
